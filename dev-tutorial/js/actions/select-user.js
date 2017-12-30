3724dc02e07978a8fc258708bde28f6fd9b01772
Suyash Khandwe (etw) <suyash.khandwe@nike.com>
2017-12-29 16:52:31 -0800

    Actions and Action Creators
    Tutorial completed - https://www.youtube.com/watch?v=_x3gitcwtAc

---
+++ dev-tutorial/js/actions/select-user.js
@@ -0,0 +1,8 @@
+/*export default function = (user) => {
+  console.log('Clicked - ', user.first);
+  return {
+    type: "USER_NAME_SELECTED",
+    payload: user
+  }
+};
+*/
--- dev-tutorial/js/containers/user-list.js
+++ dev-tutorial/js/containers/user-list.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {selectUser} from '../actions/index';
 
 class UserList extends Component {
 
   createListItems() {
     return this.props.users.map((user) => {
       return (
-        <li key={user.id}>{user.first} {user.last}</li>
+        <li key={user.id} onClick={() => this.props.selectUser(user)}>
+          {user.first} {user.last}
+        </li>
       );
     });
   }
@@ -27,4 +30,8 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(UserList);
+function matchDispatchToProps(dispatch) {
+  return bindActionCreators({selectUser: selectUser}, dispatch);
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(UserList);
