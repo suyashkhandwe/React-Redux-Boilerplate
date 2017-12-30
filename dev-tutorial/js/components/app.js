19a7f610f36f924b6eea09c8ffc62cdf74e634cd
Suyash Khandwe (etw) <suyash.khandwe@nike.com>
2017-12-29 17:20:05 -0800

    Finishing up...
    Tutorial completed - https://www.youtube.com/watch?v=p1UmoELah8M

--- dev-tutorial/js/components/app.js
+++ dev-tutorial/js/components/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserList from '../containers/user-list'
+import UserDetails from '../containers/user-details'
 require('../../scss/style.scss');
 //As in case of natice React, component only returns the HTML to be rendered
 
@@ -9,6 +10,7 @@ const App = () => (
     <UserList />
     <hr/>
     <h3>User Details</h3>
+    <UserDetails />
   </div>
 );
 
