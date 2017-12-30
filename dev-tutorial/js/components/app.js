ed936826d8422a549ce8d075ea1e469ec4fab3d7
Suyash Khandwe (etw) <suyash.khandwe@nike.com>
2017-12-29 16:20:00 -0800

    Map State to Props (using containers)
    Tutorial completed - https://www.youtube.com/watch?v=IIMUXbkKzW0

--- dev-tutorial/js/components/app.js
+++ dev-tutorial/js/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import UserList from '../containers/user-list'
 require('../../scss/style.scss');
 //As in case of natice React, component only returns the HTML to be rendered
 
 const App = () => (
   <div>
     <h2>User Name List</h2>
+    <UserList />
     <hr/>
     <h3>User Details</h3>
   </div>
--- dev-tutorial/js/index.js
+++ dev-tutorial/js/index.js
@@ -1,8 +1,8 @@
 import 'babel-polyfill'; //Makes code backwards compatible for older browsers.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore} from 'redux'; //Module to create store :)
+import {Provider} from 'react-redux'; //Module to create providers.
+import {createStore} from 'redux'; //Module to create store.
 //Wrapping 'createStore' in curly brackets allows auto-creation of function that is ready to use
 import allReducers from './reducers';
 import App from './components/app'
